edc4ab80f8d8ccf78a0d989ecc4a88f76eae22dd
Stanislav Lukyanov <lukyanov.stas@mail.ru>
2023-03-05 20:27:07 +0400

    remove header unnecessary args

--- pay_methods/unistream.py
+++ pay_methods/unistream.py
@@ -8,18 +8,9 @@ UNISTREAM_HEADERS = {
     'accept': 'application/json, text/javascript, */*; q=0.01',
     'accept-language': 'ru',
     'authorization': 'Bearer {}'.format(BEARER_UNISTREAM_TOKEN),
-    'cache-control': 'no-cache',
     'dnt': '1',
     'origin': 'https://unistream.ru',
-    'pragma': 'no-cache',
     'referer': 'https://unistream.ru/',
-    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
 }
 
 
